ad3b40d2c40add6e6248cc132442f83e3b351149
DhirajGazalwar <d.gazalwar93@gmail.com>
2019-09-20 15:59:52 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+import mysql.connector
+mydb
+=mysql.connector.connect(host="mysql",user="root",passwd="password",auth_plugin="mysql_native_password")
+testcursor=mydb.cursor()
+testcursor.execute("CREATE DATABASE studentdb")
+testcursor.execute("SHOW DATABASES")
+
+for x in testcursor:
+        print(x)
